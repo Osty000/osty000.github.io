ed161b5049c5cf36857b6aa08d460d00715049f5
Danya <kazakdana2006@gmail.com>
2020-11-06 00:48:38 +0200

    1

---
+++ assets/js/script15.js
@@ -0,0 +1,19 @@
+var numberStrings15=["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"];
+
+function updateNumberList(array){
+  var s="";
+  for(var i=0;i<array.length;i++){
+    s+="<br>"+array[i];
+  }
+  document.getElementById("numberList").innerHTML=s;
+}
+
+function shuffleUsingRandomSwapping(array){
+  var j,x,i=0,len=array.length;
+  for(i;i<len;i++){
+    j=Math.floor(Math.random()*len);
+    x=array[i];
+    array[i]=array[j];
+    array[j]=x;
+  }
+};
---
+++ assets/js/script9.js
@@ -0,0 +1,19 @@
+var numberStrings9=["1","2","3","4","5","6","7","8","9"];
+
+function updateNumberList(array){
+  var s="";
+  for(var i=0;i<array.length;i++){
+    s+="<br>"+array[i];
+  }
+  document.getElementById("numberList").innerHTML=s;
+}
+
+function shuffleUsingRandomSwapping(array){
+  var j,x,i=0,len=array.length;
+  for(i;i<len;i++){
+    j=Math.floor(Math.random()*len);
+    x=array[i];
+    array[i]=array[j];
+    array[j]=x;
+  }
+};
